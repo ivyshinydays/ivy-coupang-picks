1f6538fddff811ac5c6b029c55439b7abed8774d
ivyshinydays <ivyshinydays@gmail.com>
2026-09-15 13:21:13 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='ivy-picks-v1';
-const ASSETS=['./','./index.html','./open.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{ if(new URL(e.request.url).origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));} });
